516adccb0e2a100816bd238ba4cc0e3dcd17caa8
Lucas Cortés <lucascortes@me.com>
2018-05-08 20:46:19 -0300

    continue

--- client/src/app/chat/sidebar/sidebar.component.ts
+++ client/src/app/chat/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-	private currentTab: string = "users";
+	private currentTab: string = "groups";
 
   constructor() { }
 
